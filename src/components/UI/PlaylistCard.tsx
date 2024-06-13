1ae35bcc0238918384acf21b80ae99453316a3f0
guillaumeS <guillaume.saurin@ynov.com>
2024-06-13 14:36:40 +0200

    feature/gestion-playlists : - Gestion update playlists

--- src/components/UI/PlaylistCard.tsx
+++ src/components/UI/PlaylistCard.tsx
@@ -7,16 +7,18 @@ import {Playlist} from "../../utils/interface.ts";
 import ModalPlaylistInfo from "../Modal/ModalPlaylistInfo.tsx";
 
 interface Inputs extends InputProps {
-   playlist: Playlist;
+    playlist: Playlist;
+    updatePlaylistEvent: CallableFunction;
 }
 
 const PlaylistCardComponent: React.FC<Inputs> = ({
-                                                           playlist,
-                                                       }) => {
+                                                     playlist,
+                                                     updatePlaylistEvent
+                                                 }) => {
 
-    const [isModalUserIOpen, setIsModalUserIOpen] = useState(false);
+    const [isModalUserIOpen,setIsModalUserIOpen] = useState(false);
 
-    const handlePlaylistEditClick = (id: string) => {
+    const handlePlaylistEditClick = () => {
         return () => {
             setIsModalUserIOpen(true);
         };
@@ -24,51 +26,43 @@ const PlaylistCardComponent: React.FC<Inputs> = ({
 
     const items: MenuProps['items'] = [
         {
-            label: <div>Ajouter un titre</div>,
+            label: <a target={"_blank"} href={playlist.external_urls.spotify}>Voir la playlist</a>,
             key: '0',
         },
         {
             label: <div>Modifier</div>,
             key: '1',
-            onClick: handlePlaylistEditClick(playlist.id || '')
-        },
-        {
-            type: 'divider',
-        },
-        {
-            label: <div>Supprimer</div>,
-            key: '3',
+            onClick: handlePlaylistEditClick()
         },
     ];
 
     return (
         <Space wrap>
-        <Card
-            style={{
-                width: 220,
-                boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)'
-            }}
-            cover={playlist.images[0].url &&
-                <img alt={playlist.name} src={playlist.images[0].url} style={{width: '220px',height: '220px'}}/>
-            }
-        >
-            {playlist.description ? <Meta title={playlist.name} style={{whiteSpace: 'nowrap'}}
-                                 description={playlist.description}/> :
-                <Meta title={playlist.name} description={playlist.tracks.total + " titres"}/>}
-            <Dropdown menu={{items}} trigger={['click']}>
-                <a onClick={(e) => e.preventDefault()}>
-                    <Space>
-                        <span>...</span>
-                    </Space>
-                </a>
-            </Dropdown>
-        </Card>
+            <Card
+                style={{
+                    width: 220,
+                    boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)'
+                }}
+                cover={playlist.images[0].url &&
+                    <img alt={playlist.name} src={playlist.images[0].url} style={{width: '220px',height: '220px'}}/>
+                }
+            >
+                {playlist.description ? <Meta title={playlist.name} style={{whiteSpace: 'nowrap'}}
+                                              description={playlist.description}/> :
+                    <Meta title={playlist.name} description={playlist.tracks.total + " titres"}/>}
+                <Dropdown menu={{items}} trigger={['click']}>
+                    <a onClick={(e) => e.preventDefault()}>
+                        <Space>
+                            <span>...</span>
+                        </Space>
+                    </a>
+                </Dropdown>
+            </Card>
             <ModalPlaylistInfo
                 playlist={playlist}
-                isModalOpen={isModalUserIOpen}
-                setIsModalOpen={setIsModalUserIOpen}
-                onClose={() => {}}
+                setOpen={setIsModalUserIOpen}
                 open={isModalUserIOpen}
+                onPlaylistAdded={(playlistName, playlistDescription, playlistIsPublic, playlistId) => updatePlaylistEvent(playlistName, playlistDescription, playlistIsPublic, playlistId)}
             />
         </Space>
     );
--- src/containers/playlists-management/PlaylistsManagement.tsx
+++ src/containers/playlists-management/PlaylistsManagement.tsx
@@ -4,12 +4,10 @@ import './PlaylistsManagement.css';
 import PlaylistCard from "../../components/UI/PlaylistCard.tsx";
 import {Playlist} from "../../utils/interface.ts";
 
-
-
-
 const PlaylistsManagement: React.FC = () => {
     const token = window.localStorage.getItem('access_token');
     const [playlists,setPlaylists] = useState<Playlist[]>([]);
+    const [isUpdated,setIsUpdated] = useState<boolean>(false);
     useEffect(() => {
         if (!token) return;
 
@@ -18,7 +16,7 @@ const PlaylistsManagement: React.FC = () => {
                 const playlistsResponse = await fetch('https://api.spotify.com/v1/me/playlists',{
                     headers: {'Authorization': `Bearer ${token}`}
                 });
-                if (!playlistsResponse.ok) throw new Error('Failed to fetch artists');
+                if (!playlistsResponse.ok) throw new Error('Failed to fetch playlists');
                 const playlistsData = await playlistsResponse.json();
 
                 setPlaylists(playlistsData.items);
@@ -27,7 +25,45 @@ const PlaylistsManagement: React.FC = () => {
             }
         };
         fetchData();
-    },[token]);
+    },[isUpdated]);
+
+    const updatePlaylist = async (playlistName: string, playlistDescription: string, playlistIsPublic: boolean, playlistId: string) => {
+        console.log(playlistName,playlistDescription,playlistIsPublic,playlistId)
+        try {
+            const updateResponse = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}`, {
+                method: 'PUT',
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    name: playlistName,
+                    description: playlistDescription,
+                    public: playlistIsPublic
+                })
+            });
+
+            if (!updateResponse.ok) throw new Error('Failed to update playlist');
+
+            // After updating the playlist, fetch the updated data
+            const fetchData = async () => {
+                try {
+                    const playlistsResponse = await fetch('https://api.spotify.com/v1/me/playlists', {
+                        headers: { 'Authorization': `Bearer ${token}` }
+                    });
+                    if (!playlistsResponse.ok) throw new Error('Failed to fetch playlists');
+                    const playlistsData = await playlistsResponse.json();
+                    setPlaylists(playlistsData.items);
+                } catch (error) {
+                    console.error(error);
+                }
+            };
+            fetchData();
+
+        } catch (error) {
+            console.error(error);
+        }
+    }
     return (
         <><title>SpotiFlow • Playlists Management</title>
 
@@ -43,6 +79,7 @@ const PlaylistsManagement: React.FC = () => {
                             <PlaylistCard
                                 key={index}
                                 playlist={playlist}
+                                updatePlaylistEvent={updatePlaylist}
                             />
                         ))}
                     </div>
